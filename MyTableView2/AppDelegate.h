3f20e9cd23db064e5255190df172ef48ae1b9b29
logen525 <zhengjianjun@zhengmatoiMac.local>
2013-06-04 13:32:46 +0800

    test

---
+++ MyTableView2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyTableView2
+//
+//  Created by person on 13-6-4.
+//  Copyright (c) 2013年 cw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyTableView2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyTableView2
+//
+//  Created by person on 13-6-4.
+//  Copyright (c) 2013年 cw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
